d2d617479299edf73563838c440d2558fa25d525
PutriHabibahDevina <putrihdm13@gmail.com>
2024-12-28 17:32:39 +0700

    Initial commit

---
+++ app/src/main/java/com/example/praktikum12/service/MahasiswaService.kt
@@ -0,0 +1,32 @@
+package com.example.praktikum12.service
+
+import com.example.praktikum12.model.Mahasiswa
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Query
+import retrofit2.http.Headers
+
+interface MahasiswaService {
+    @Headers(
+        "Accept: application/json",
+        "Content-Type: application/json"
+    )
+    @GET("bacamahasiswa.php")
+    suspend fun getMahasiswa(): List<Mahasiswa>
+
+    @GET("bacama1hasiswa.php/{nim}")
+    suspend fun getMahasiswaById(@Query("nim") nim:String): Mahasiswa
+
+    @POST("insertmahasiswa.php")
+    suspend fun insertMahasiswa(@Body mahasiswa: Mahasiswa)
+
+    @PUT("editmahasiswa.php/{nim}")
+    suspend fun updateMahasiswa(@Query("nim") nim:String, @Body mahasiswa: Mahasiswa)
+
+    @DELETE("deletemahasiswa.php/{nim}")
+    suspend fun deleteMahasiswa(@Query("nim") nim: String): Response<Void>
+}
