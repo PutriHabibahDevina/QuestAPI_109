5364019d22c6b909fd39c2e55ae6393db677ad9e
PutriHabibahDevina <putrihdm13@gmail.com>
2024-12-30 13:43:00 +0700

    Menambahkan destinasi navigasi untuk home, detail, entry, dan update

--- app/src/main/java/com/example/praktikum12/navigasi/DestinasiNavigasi.kt
+++ app/src/main/java/com/example/praktikum12/navigasi/DestinasiNavigasi.kt
@@ -1,6 +1,30 @@
 package com.example.praktikum12.navigasi
 
-interface DestinasiNavigasi{
+interface  DestinasiNavigasi{
     val route: String
     val titleRes: String
+}
+
+object DestinasiHome: DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = "Home Mhs"
+}
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route: String = "detail/{nim}"
+    override val titleRes: String = "Detail Mhs"
+
+    fun createRoute(nim: String): String {
+        return "detail/$nim"
+    }
+}
+
+object DestinasiEntry : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
+
+object DestinasiUpdate : DestinasiNavigasi {
+    override val route: String = "update/{nim}"
+    override val titleRes: String = "Update Mhs"
 }
